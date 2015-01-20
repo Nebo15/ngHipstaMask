111ae3507ea6e67d383fe02d213be0f51168ac08
Alexey Bondarenko <alexeybondarenko@me.com>
2015-01-20 19:16:04 +0200

    Added all API methods to  service. Added  methods to configure service before injecting to other objects.

--- src/mbank.lib.angular/mbank.lib.angular.js
+++ src/mbank.lib.angular/mbank.lib.angular.js
@@ -14,8 +14,11 @@ var Mbank = angular.module('mbank.lib.angular',
         'mbank.lib.angular.config'
     ]);
 
-Mbank.config(['$httpProvider', function($httpProvider) {
+Mbank.config(['$httpProvider','$mbankApiProvider', function($httpProvider, $mbankApiProvider) {
 //    $httpProvider.defaults.useXDomain = true;
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
 //    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+    console.log('$mbankApiProvider', $mbankApiProvider);
+    $mbankApiProvider.set('url', 'http://0.0.0.0:1234/api.mbank.nebo15.me/v1/');
 }]);
